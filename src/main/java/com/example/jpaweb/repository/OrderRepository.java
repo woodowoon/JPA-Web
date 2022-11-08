87290dcbc69e073258fdd2176d3e49c39b65b8b8
dowoon <woodw920@gmail.com>
2022-11-08 17:29:06 +0900

    #9 - 주문 엔티티, 리포지토리, 서비스, 테스트 개발
    주문 엔티티, 리포지토리, 서비스, 테스트를 개발하였다.

---
+++ src/main/java/com/example/jpaweb/repository/OrderRepository.java
@@ -0,0 +1,25 @@
+package com.example.jpaweb.repository;
+
+import com.example.jpaweb.domain.Order;
+import com.example.jpaweb.domain.OrderStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderStatus orderStatus)
+}
