89209f45065d233b88730788d368d82d00e9c210
Dowoon <woodw920@gmail.com>
2022-11-11 11:10:53 +0900

    #14 - 회원 조회 API

--- src/main/java/com/example/jpaweb/api/MemberApiController.java
+++ src/main/java/com/example/jpaweb/api/MemberApiController.java
@@ -10,11 +10,40 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members") // 회원 등록 v1 api : 엔티티를 그대로 받는 api (사용을 금할것.)
     public CreateMemberReponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -35,14 +64,14 @@ public class MemberApiController {
         return new CreateMemberReponse(id);
     }
 
-    @PatchMapping("/api/v2/members/{id}")
+    @PatchMapping("/api/v2/members/{id}") // 회원 수정 api
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
-            @RequestBody @Valid UpdateMemberRequest request) {
+            @RequestBody @Valid UpdateMemberRequest request) { // 변경할 name 을 가져온다.
 
-        memberService.update(id, request.getName());
-        Member findMember = memberService.findOne(id);
-        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+        memberService.update(id, request.getName()); // 수정
+        Member findMember = memberService.findOne(id); // find 하는 이유는 화면에 return 값을 뿌려주기 위해서 find 해준다
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName()); // 화면에 뿌려주는 값이다.
     }
 
     @Data
