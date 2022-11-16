bbb3a2490f9aa367132f74ffcea1627031a2310d
Dowoon <woodw920@gmail.com>
2022-11-16 11:23:09 +0900

    #17 - 주문조회 V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/com/example/jpaweb/api/OrderApiController.java
+++ src/main/java/com/example/jpaweb/api/OrderApiController.java
@@ -82,6 +82,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     static class OrderDto {
         private Long orderId;
--- src/main/java/com/example/jpaweb/repository/order/query/OrderQueryRepository.java
+++ src/main/java/com/example/jpaweb/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,6 +23,39 @@ public class OrderQueryRepository {
         });
 
         return result;
+    }
+
+    // 진짜 미쳤다 쿼리문 2개나가고 쿼리문 개적게나가 In 의 효과인가???
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = toOrderIds(result);
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId()))); // 메모리에 있는 값을 돌려주기 때문에 쿼리가 많이 나가지 않는다.
+
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery( // 쿼리를 한번 실행하고.  // 쿼리 2
+                        "select new com.example.jpaweb.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream() // 메모리에 맵으로 저장한 다음에
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        return orderItemMap;
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream() // 쿼리 1번
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+        return orderIds;
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
